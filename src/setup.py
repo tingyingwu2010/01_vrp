147595c6a1897e1cfd7604d900a551852e372aef
Stephen Mak <stephen.mak18@gmail.com>
2020-09-21 16:09:03 +0100

    Added barebones folder structure, ready to develop with OpenAI Gym.

---
+++ src/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='vrp',
+      version='0.0.1',
+      install_requires=['gym', 'tensorflow']  # And any other dependencies gridworldEnv needs
+)
---
+++ src/vrp/envs/vrp_env.py
@@ -0,0 +1,31 @@
+import gym
+
+class VRPEnv(gym.Env):
+    """This class replicates the CVRP Environment from (Nazari et. al 2018).
+        https://arxiv.org/abs/1802.04240
+        
+        
+    Attributes:
+        n_destinations (int): An integer indicating number of destinations
+    """
+
+    def __init__(self):
+        """Inits VRPEnv with n_destinations"""
+        super(VRPEnv, self).__init__()
+        self.n_destinations = 10
+
+    def step(self, action):
+        """Takes a step using `action`."""
+        raise NotImplementedError
+
+    def reset(self):
+        """Performs operation blah."""
+        raise NotImplementedError
+    
+    def render(self):
+        """Performs operation blah."""
+        raise NotImplementedError
+    
+    def close(self):
+        """Performs operation blah."""
+        raise NotImplementedError
