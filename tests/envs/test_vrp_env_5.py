b7cc3566c4392faf42515a60ed8576a50874c5fa
Stephen Mak <stephen.mak18@gmail.com>
2020-09-22 12:42:01 +0100

    Fixed fixture to work properly now

--- tests/envs/test_vrp_env_5.py
+++ tests/envs/test_vrp_env_5.py
@@ -2,6 +2,9 @@ import pytest
 import gym # type: ignore
 from vrp.envs.vrp_env_5 import VRPEnv5 # type: ignore
 
+
+import sm_functions as sm # type: ignore
+
 def summation(a: int, b: int) -> int:
     return a + b
 
@@ -13,11 +16,15 @@ class TestVrpEnv(object):
         a given seed. Usually use 123456789 (make sure to use a large number
         for the seed)'''
         def _make_env(seed):
-            sm.random_seed(seed)
+            sm.set_seed(seed)
             env = gym.make('vrp-v5')
             return env
         return _make_env
     
+    def test_fixture(self, generate_env):
+        env = generate_env(123456789)
+        assert 1
+    
     def test_init(self):
         env = gym.make('vrp-v5')
-        assert True
+        assert 1
